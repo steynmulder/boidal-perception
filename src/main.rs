26fe72d1ea39004c6af7d8bda2e4dd999e9af4d5
Steyn Mulder <steynmulder@gmail.com>
2025-01-13 16:01:25 +0100

    implemented simple noise addition for speed actuation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::{collections::HashMap, ops::Div};
 use macroquad::prelude::*;
 
 use miniquad::window::set_window_size;
+
 use ::rand::*;
+use rand_distr::{Normal, Distribution};
 
 const CANVAS_WIDTH : u32 = 1400;
 const CANVAS_HEIGHT : u32 = 700;
@@ -28,10 +30,11 @@ const OBSTACLE_MIN_RADIUS: f32 = 10.0;
 const OBSTACLE_MAX_RADIUS: f32 = 25.0;
 
 
-
 struct Boid {
-    x: f32,
-    y: f32,
+    x: f32, // actual x
+    y: f32, // actual y
+    xz: f32, // what robot thinks is x
+    yz: f32, // what robot thinks is y
     width: f32,
     height: f32,
     dx: f32,
@@ -93,9 +96,13 @@ impl Obstacle {
 
 impl Boid {
     pub fn create() -> Boid {
+        let x = thread_rng().gen_range(CANVAS_WIDTH as f32 / 5.0..4.0 * CANVAS_WIDTH as f32 / 5.0);
+        let y = thread_rng().gen_range(CANVAS_HEIGHT as f32 / 5.0..4.0 * CANVAS_HEIGHT as f32 / 5.0);
         Boid {
-            x: thread_rng().gen_range(CANVAS_WIDTH as f32 / 5.0..4.0 * CANVAS_WIDTH as f32 / 5.0),
-            y: thread_rng().gen_range(CANVAS_HEIGHT as f32 / 5.0..4.0 * CANVAS_HEIGHT as f32 / 5.0),
+            x: x,
+            y: y,
+            xz: x,
+            yz: y,
             width: BOID_WIDTH,
             height: BOID_WIDTH,
             dx: thread_rng().gen_range(-2.5..=2.5),
@@ -104,7 +111,8 @@ impl Boid {
         }
     }
 
-    pub fn update(&mut self, segments: &HashMap<Segment, Vec<BoidPosVel>>, obstacles: &Vec<Obstacle>) -> Segment {
+    pub fn update(&mut self, segments: &HashMap<Segment, Vec<BoidPosVel>>, obstacles: &Vec<Obstacle>, normal_dist_speed: &Normal<f32>) -> Segment {
+        // optimization of checks: use actual coordinates
         let min_seg_x = ((self.x - VISUAL_RANGE).max(0.0) as u32).div(SEGMENT_WIDTH);
         let max_seg_x = ((self.x + VISUAL_RANGE).min(CANVAS_WIDTH as f32) as u32).div(SEGMENT_WIDTH);
         let min_seg_y = ((self.y - VISUAL_RANGE).max(0.0) as u32).div(SEGMENT_WIDTH);
@@ -122,8 +130,9 @@ impl Boid {
         for i in min_seg_x..=max_seg_x {
             for j in min_seg_y..=max_seg_y {
                 for point in segments.get(&Segment{x: i, y: j}).unwrap_or(&Vec::new()) {
-                    let dx = self.x - point.x;
-                    let dy = self.y - point.y;
+                    // use noisy coordinates here
+                    let dx = self.xz - point.x;
+                    let dy = self.yz - point.y;
 
                     if (dx.powi(2) + dy.powi(2)).sqrt() < SPACE_RANGE {
                         avoid_dx += dx;
@@ -155,11 +164,11 @@ impl Boid {
         self.dx += avoid_dx * AVOID;
         self.dy += avoid_dy * AVOID;
 
-
-        if self.x + self.dx + self.width > RIGHT_MARGIN && self.dx > 0.0 {self.dx -= TURNING_FACTOR;}
-        if self.y + self.dy + self.height > BOTTOM_MARGIN && self.dy > 0.0 {self.dy -= TURNING_FACTOR;}
-        if self.x + self.dx < LEFT_MARGIN && self.dx < 0.0 {self.dx += TURNING_FACTOR;}
-        if self.y + self.dy < TOP_MARGIN && self.dy < 0.0 {self.dy += TURNING_FACTOR;}
+        // use noisy coordinates
+        if self.xz + self.dx + self.width > RIGHT_MARGIN && self.dx > 0.0 {self.dx -= TURNING_FACTOR;}
+        if self.yz + self.dy + self.height > BOTTOM_MARGIN && self.dy > 0.0 {self.dy -= TURNING_FACTOR;}
+        if self.xz + self.dx < LEFT_MARGIN && self.dx < 0.0 {self.dx += TURNING_FACTOR;}
+        if self.yz + self.dy < TOP_MARGIN && self.dy < 0.0 {self.dy += TURNING_FACTOR;}
 
         for obs in obstacles.iter() {
 
@@ -168,28 +177,43 @@ impl Boid {
             if margins.is_none() {continue;}
             let result = margins.unwrap();
             if result < OBSTACLE_MARGIN {
-                self.dx = -1.0 * (obs.x - self.x);
-                self.dy = -1.0 * (obs.y - self.y);
+                // use noisy coordinates
+                self.dx = -1.0 * (obs.x - self.xz);
+                self.dy = -1.0 * (obs.y - self.yz);
             } else {
-                self.dx += -1.0 * (obs.x - self.x) * AVOID * TURNING_FACTOR;
-                self.dy += -1.0 * (obs.y - self.y) * AVOID * TURNING_FACTOR;
+                self.dx += -1.0 * (obs.x - self.xz) * AVOID * TURNING_FACTOR; // TODO switch this up with first?
+                self.dy += -1.0 * (obs.y - self.yz) * AVOID * TURNING_FACTOR;
             }
 
         }
 
+        // noisy movement with ideal speed actuation
         let speed = (self.dx.powi(2) + self.dy.powi(2)).sqrt();
 
-        self.dx = (self.dx / speed) * SPEED;
-        self.dy = (self.dy / speed) * SPEED;
+        self.xz += (self.dx / speed) * SPEED;
+        self.yz += (self.dy / speed) * SPEED;
+
+        // actual movement with noise in speed actuation
+
+        let dx_samp = normal_dist_speed.sample(&mut thread_rng());
+        let dy_samp = normal_dist_speed.sample(&mut thread_rng());
+
+        let speed_actual = ((self.dx + dx_samp).powi(2) + (self.dy + dy_samp).powi(2)).sqrt();
+
+        self.dx = ((self.dx + dx_samp) / speed_actual) * SPEED;
+        self.dy = ((self.dy + dy_samp) / speed_actual) * SPEED;
 
         self.x += self.dx;
         self.y += self.dy;
+
+        // use actual coordinates to keep within the bounds
 
         if self.x + self.width > CANVAS_WIDTH as f32 {self.x = CANVAS_WIDTH as f32 - self.width - 1.0;}
         if self.y + self.height > CANVAS_HEIGHT as f32 {self.y = CANVAS_HEIGHT as f32 - self.height - 1.0;}
         if self.x < 0.0 {self.x = 1.0;}
         if self.y < 0.0 {self.y = 1.0;}
 
+        // optimization of checks: use actual coordinates
 
         Segment{x: (self.x as u32).div(SEGMENT_WIDTH), y: (self.y as u32).div(SEGMENT_WIDTH)}
     }
@@ -226,8 +250,9 @@ async fn main(){
     set_window_size(CANVAS_WIDTH, CANVAS_HEIGHT);
 
     let mut boids = (0..NUMBER_BOIDS).into_iter().map(|_| Boid::create()).collect::<Vec<_>>();
-
     let obstacles = (0..NUMBER_SQUARE_OBSTACLES).into_iter().map(|_| Obstacle::create()).collect::<Vec<_>>();
+
+    let normal_dist_speed= Normal::new(0.0, SPEED / 3.0).unwrap();
 
     let mut prev_pos_segments : HashMap<Segment, Vec<BoidPosVel>> = HashMap::new();
 
@@ -243,7 +268,7 @@ async fn main(){
         let mut pos_segments : HashMap<Segment, Vec<BoidPosVel>> = HashMap::new();
 
         for boid in boids.iter_mut() {
-            pos_segments.entry(boid.update(&prev_pos_segments, &obstacles))
+            pos_segments.entry(boid.update(&prev_pos_segments, &obstacles, &normal_dist_speed))
                 .or_insert_with(Vec::new)
                 .push(BoidPosVel{x: boid.x, y: boid.y, dx: boid.dx, dy: boid.dy});
             boid.draw();
